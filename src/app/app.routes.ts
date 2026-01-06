c5a2b52688887e0488db92598ad593c18230a4a9
Pavel Novik <brn2befree@gmail.com>
2026-01-06 13:49:41 +0300

    add routs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Users } from './components/users/users';
+import { Todolists } from './components/todolists/todolists';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+    title: 'Home',
+  },
+  {
+    path: 'users',
+    component: Users,
+    title: 'Users',
+  },
+  {
+    path: 'todolists',
+    component: Todolists,
+    title: 'Todolists',
+  },
+];
