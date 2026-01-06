ae6726df153c67ca89c3f71aa540ca79f948324a
Pavel Novik <brn2befree@gmail.com>
2026-01-06 13:50:07 +0300

    create components

---
+++ src/app/components/todolists/todolists.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tl-todolists',
+  imports: [],
+  templateUrl: './todolists.html',
+  styleUrl: './todolists.scss',
+})
+export class Todolists {
+
+}
