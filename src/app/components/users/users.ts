3742cc89f0c1a9eecf5357eea38a4e8b686c8026
Pavel Novik <brn2befree@gmail.com>
2026-01-07 22:00:35 +0300

    add users in component

--- src/app/components/users/users.ts
+++ src/app/components/users/users.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { User } from './service/user';
+import { Observable } from 'rxjs';
+import { UserT } from '../../shared/types';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'tl-users',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './users.html',
   styleUrls: ['./users.scss'],
 })
-export class Users {}
+export class Users {
+  users$!: Observable<UserT[]>;
+  private userService = inject(User);
+
+  getUsers(page: number) {
+    this.users$ = this.userService.getUsers(page);
+  }
+
+  ngOnInit() {
+    this.getUsers(1);
+  }
+}
