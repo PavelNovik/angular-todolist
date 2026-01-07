05f83dd953cd6e0c676d111c906c0cfdfbb254f7
Pavel Novik <brn2befree@gmail.com>
2026-01-07 23:02:19 +0300

    add task component

---
+++ src/app/components/todolist/task/task.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tl-task',
+  imports: [],
+  templateUrl: './task.html',
+  styleUrl: './task.scss',
+})
+export class Task {
+
+}
