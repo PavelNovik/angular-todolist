ace6a3a4a613c48031f8ea73f06b153b70021ad6
Pavel Novik <brn2befree@gmail.com>
2026-01-07 23:03:00 +0300

    add todolist in template with data

--- src/app/components/todolist/todolist.ts
+++ src/app/components/todolist/todolist.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TodolistT } from '../../shared/types';
+import { TodolistService } from './service/todolist.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'tl-todolist',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './todolist.html',
   styleUrls: ['./todolist.scss'],
 })
-export class Todolist {}
+export class Todolist implements OnInit {
+  todolist$!: Observable<TodolistT[]>;
+  private todolistService = inject(TodolistService);
+  getTodolists() {
+    this.todolistService.getTodolists();
+  }
+
+  ngOnInit() {
+    this.todolist$ = this.todolistService.todolists$;
+    this.getTodolists();
+  }
+}
