36e05c72f8aebf9d846aff0744ccefb68f52c952
Pavel Novik <brn2befree@gmail.com>
2026-01-08 15:34:43 +0300

    add styles and button for pagination

--- src/app/components/users/users.ts
+++ src/app/components/users/users.ts
@@ -2,23 +2,34 @@ import { Component, inject, OnInit } from '@angular/core';
 import { UserService } from './service/user.service';
 import { Observable } from 'rxjs';
 import { UserT } from '../../shared/types';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, NgOptimizedImage } from '@angular/common';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'tl-users',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, NgOptimizedImage, RouterLink],
   templateUrl: './users.html',
   styleUrls: ['./users.scss'],
 })
 export class Users implements OnInit {
   users$!: Observable<UserT[]>;
   private userService = inject(UserService);
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
 
   getUsers(page: number) {
     this.users$ = this.userService.getUsers(page);
   }
 
+  protected nextUserHandler() {
+    const page = Number(this.route.snapshot.queryParamMap.get('page'));
+    const nextPage = page ? page + 1 : 2;
+    this.router.navigate(['users'], { queryParams: { page: nextPage } });
+  }
+
   ngOnInit() {
-    this.getUsers(1);
+    this.route.queryParams.subscribe((params) => {
+      this.getUsers(params['page']);
+    });
   }
 }
