c7bd11f6b923cf592ff06b28b4b58cde12b83746
Pavel Novik <brn2befree@gmail.com>
2026-01-09 12:15:41 +0300

    remove optional method

--- src/app/components/todolist/todolist.ts
+++ src/app/components/todolist/todolist.ts
@@ -3,25 +3,20 @@ import { Observable } from 'rxjs';
 import { TodolistT } from '../../shared/types';
 import { TodolistService } from './service/todolist.service';
 import { AsyncPipe } from '@angular/common';
-import { TaskService } from './service/task.service';
-import { Task } from './task/task';
+import { Todo } from './todo/todo';
 
 @Component({
   selector: 'tl-todolist',
-  imports: [AsyncPipe, Task],
+  imports: [AsyncPipe, Todo],
   templateUrl: './todolist.html',
   styleUrls: ['./todolist.scss'],
 })
 export class Todolist implements OnInit {
   todolist$!: Observable<TodolistT[]>;
   private todolistService = inject(TodolistService);
-  private taskService = inject(TaskService);
-  getTodolists() {
-    this.todolistService.getTodolists();
-  }
 
   ngOnInit() {
     this.todolist$ = this.todolistService.todolists$;
-    this.getTodolists();
+    this.todolistService.getTodolists();
   }
 }
