a01508c2b72ef0b4bf3af6e3b45feab7fa573798
Pavel Novik <brn2befree@gmail.com>
2026-01-10 15:00:27 +0300

    add method in todolist service for create todolist

--- src/app/components/todolist/todolist.ts
+++ src/app/components/todolist/todolist.ts
@@ -4,16 +4,19 @@ import { TodolistT } from '../../shared/types';
 import { TodolistService } from './service/todolist.service';
 import { AsyncPipe } from '@angular/common';
 import { Todo } from './todo/todo';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'tl-todolist',
-  imports: [AsyncPipe, Todo],
+  imports: [AsyncPipe, Todo, FormsModule],
   templateUrl: './todolist.html',
   styleUrls: ['./todolist.scss'],
 })
 export class Todolist implements OnInit {
   todolist$!: Observable<TodolistT[]>;
   private todolistService = inject(TodolistService);
+  protected todoTitle = '';
+  protected addTodolisthandler() {}
 
   ngOnInit() {
     this.todolist$ = this.todolistService.todolists$;
