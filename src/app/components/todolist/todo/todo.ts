a3815a6ab9bada51970724c9a4aaf6d6a30334cb
Pavel Novik <brn2befree@gmail.com>
2026-01-11 10:03:22 +0300

    add edit mode in component

--- src/app/components/todolist/todo/todo.ts
+++ src/app/components/todolist/todo/todo.ts
@@ -1,21 +1,44 @@
 import { Component, input, output } from '@angular/core';
 import { TodolistT } from '../../../shared/types';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'tl-todo',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './todo.html',
   styleUrl: './todo.scss',
 })
 export class Todo {
   readonly todolist = input<TodolistT>();
   readonly removeTodo = output<string>();
+  readonly editTodo = output<{ id: string; newTitle: string }>();
   // @Output() readonly removeTodoEvent = new EventEmitter<string>();
+
+  isEditMode = false;
+  newTitle = '';
+
+  activateEditModeHandler() {
+    const title = this.todolist()?.title;
+    if (title) {
+      this.newTitle = title;
+    }
+    this.isEditMode = true;
+  }
 
   protected removeTodolist() {
     const id = this.todolist()?.id;
     if (id) {
       this.removeTodo.emit(id);
     }
+  }
+
+  protected editTitleHandler() {
+    const id = this.todolist()?.id;
+    if (id) {
+      this.editTodo.emit({ id, newTitle: this.newTitle });
+    }
+
+    this.isEditMode = false;
+    this.newTitle = '';
   }
 }
