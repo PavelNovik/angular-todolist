21e19716c1c6be70be9d855ac3a5c262dbff1b66
Pavel Novik <brn2befree@gmail.com>
2026-01-14 00:05:06 +0300

    add get task service

--- src/app/components/todolist/service/task.service.ts
+++ src/app/components/todolist/service/task.service.ts
@@ -2,24 +2,25 @@ import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { BeautyLogger } from '../../../shared/beauty-logger';
-import { BehaviorSubject, catchError, EMPTY, Observable } from 'rxjs';
-import { Tasks, TaskT } from '../../../shared/types';
+import { catchError, EMPTY, map, Observable } from 'rxjs';
+import { TasksResponse } from '../../../shared/types';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  tasks$: BehaviorSubject<TaskT[]> = new BehaviorSubject<TaskT[]>([]);
+  // tasks$: BehaviorSubject<TaskT[]> = new BehaviorSubject<TaskT[]>([]);
   httpAddress = `${environment.baseUrl}/1.1/todo-lists`;
 
   private http = inject(HttpClient);
   private beautyLogger = inject(BeautyLogger);
 
-  getTasks(todolistId: string | undefined) {
-    this.http
-      .get<Tasks>(`${this.httpAddress}/${todolistId}/tasks`)
+  getTasks(todolistId: string) {
+    return this.http
+      .get<TasksResponse>(`${this.httpAddress}/${todolistId}/tasks`)
       .pipe(catchError(this.errorHandler.bind(this)))
-      .subscribe((res) => this.tasks$.next(res.items));
+      .pipe(map((res) => res.items));
+    // .subscribe((res) => this.tasks$.next(res.items));
   }
   private errorHandler(error: HttpErrorResponse): Observable<never> {
     this.beautyLogger.log(error.message, 'error');
