b85b3053d2f77ddd71d7efbd7669166ae4e0f16a
Pavel Novik <brn2befree@gmail.com>
2026-01-15 01:25:44 +0300

    change state and add service for adding task

--- src/app/components/todolist/service/task.service.ts
+++ src/app/components/todolist/service/task.service.ts
@@ -2,25 +2,42 @@ import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { BeautyLogger } from '../../../shared/beauty-logger';
-import { catchError, EMPTY, map, Observable } from 'rxjs';
-import { TasksResponse } from '../../../shared/types';
+import { BehaviorSubject, catchError, EMPTY, map, Observable } from 'rxjs';
+import { BaseResponse, DomainTask, TasksResponse, TaskT } from '../../../shared/types';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  // tasks$: BehaviorSubject<TaskT[]> = new BehaviorSubject<TaskT[]>([]);
+  tasks$ = new BehaviorSubject<DomainTask>({});
   httpAddress = `${environment.baseUrl}/1.1/todo-lists`;
 
   private http = inject(HttpClient);
   private beautyLogger = inject(BeautyLogger);
 
   getTasks(todolistId: string) {
-    return this.http
+    this.http
       .get<TasksResponse>(`${this.httpAddress}/${todolistId}/tasks`)
       .pipe(catchError(this.errorHandler.bind(this)))
-      .pipe(map((res) => res.items));
-    // .subscribe((res) => this.tasks$.next(res.items));
+      .pipe(map((res) => res.items))
+      // .subscribe((res) => this.tasks$.next(res));
+      .subscribe((res) => {
+        const stateTasks = this.tasks$.getValue();
+        stateTasks[todolistId] = res;
+        this.tasks$.next(stateTasks);
+      });
+  }
+  addTask(data: { todoId: string; title: string }) {
+    this.http
+      .post<BaseResponse<{ item: TaskT }>>(`${this.httpAddress}/${data.todoId}/tasks`, {
+        title: data.title,
+      })
+      .pipe(map((res) => res.data.item))
+      .subscribe((res) => {
+        const stateTasks = this.tasks$.getValue();
+        stateTasks[data.todoId] = [res, ...stateTasks[data.todoId]];
+        this.tasks$.next(stateTasks);
+      });
   }
   private errorHandler(error: HttpErrorResponse): Observable<never> {
     this.beautyLogger.log(error.message, 'error');
