ebf325c6fb390e2ce5656cd2769322019a05af68
Pavel Novik <brn2befree@gmail.com>
2026-01-20 00:18:54 +0300

    add combineLatest for task filtration

--- src/app/components/todolist/todo/tasks/tasks.ts
+++ src/app/components/todolist/todo/tasks/tasks.ts
@@ -1,10 +1,12 @@
 import { Component, inject, input, OnInit } from '@angular/core';
 import { TaskService } from '../../service/task.service';
-import { map, Observable } from 'rxjs';
+import { combineLatest, map, Observable } from 'rxjs';
 import { TaskT, UpdateTaskModel } from '../../../../shared/types';
 import { AsyncPipe } from '@angular/common';
 import { Task } from './task/task';
 import { FormsModule } from '@angular/forms';
+import { TodolistService } from '../../service/todolist.service';
+import { TaskStatusEnum } from '../../../../enums/taskStatus.enum';
 
 @Component({
   selector: 'tl-tasks',
@@ -16,16 +18,30 @@ export class Tasks implements OnInit {
   readonly todolistId = input.required<string>();
   tasks$!: Observable<TaskT[]>;
   private taskService = inject(TaskService);
+  private todolistService = inject(TodolistService);
   protected taskTitle = '';
   ngOnInit() {
     // this.tasks$ = this.taskService.getTasks(this.todolistId());
-    this.tasks$ = this.taskService.tasks$.pipe(
+    // this.tasks$ = this.taskService.tasks$.pipe(
+    //   map((res) => {
+    //     return res[this.todolistId()];
+    //   }),
+    // );
+
+    // console.log(this.tasks$);
+
+    this.tasks$ = combineLatest([this.taskService.tasks$, this.todolistService.todolists$]).pipe(
       map((res) => {
-        return res[this.todolistId()];
+        const tasksForTodo = res[0][this.todolistId()];
+        const activeTodo = res[1].find((t) => t.id === this.todolistId());
+        return activeTodo!.filter === 'all'
+          ? tasksForTodo
+          : activeTodo!.filter === 'active'
+            ? tasksForTodo.filter((t) => t.status === TaskStatusEnum.active)
+            : tasksForTodo.filter((t) => t.status === TaskStatusEnum.completed);
       }),
     );
     this.taskService.getTasks(this.todolistId());
-    // console.log(this.tasks$);
   }
   addTaskHandler() {
     this.taskService.addTask({ todoId: this.todolistId(), title: this.taskTitle });
