630231c2062807efd83b9b964c1ed35e4abddeeb
Pavel Novik <brn2befree@gmail.com>
2026-01-19 23:54:02 +0300

    add change filter method

--- src/app/components/todolist/todo/todo.ts
+++ src/app/components/todolist/todo/todo.ts
@@ -1,16 +1,20 @@
-import { Component, input, output } from '@angular/core';
-import { TodolistT } from '../../../shared/types';
+import { Component, inject, input, output } from '@angular/core';
+import { DomainTodo, FilterType } from '../../../shared/types';
 import { FormsModule } from '@angular/forms';
 import { Tasks } from './tasks/tasks';
+import { TodoFilters } from './todo-filters/todo-filters';
+import { TodoFooter } from './todo-footer/todo-footer';
+import { TodolistService } from '../service/todolist.service';
 
 @Component({
   selector: 'tl-todo',
-  imports: [FormsModule, Tasks],
+  imports: [FormsModule, Tasks, TodoFilters, TodoFooter],
   templateUrl: './todo.html',
   styleUrl: './todo.scss',
 })
 export class Todo {
-  readonly todolist = input.required<TodolistT>();
+  readonly todolist = input.required<DomainTodo>();
+  private todolistService = inject(TodolistService);
   readonly removeTodo = output<string>();
   readonly editTodo = output<{ id: string; newTitle: string }>();
   // @Output() readonly removeTodoEvent = new EventEmitter<string>();
@@ -41,5 +45,9 @@ export class Todo {
 
     this.isEditMode = false;
     this.newTitle = '';
+  }
+
+  protected changeFilter(filter: FilterType) {
+    this.todolistService.changeFilter({ todoId: this.todolist!().id, filter });
   }
 }
