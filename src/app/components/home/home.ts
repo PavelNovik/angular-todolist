0e89f4a5aac2a579519a092a67d9d000f355895b
Pavel Novik <brn2befree@gmail.com>
2026-01-20 16:29:38 +0300

    add button logout

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'tl-home',
@@ -6,4 +7,9 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrls: ['./home.scss'],
 })
-export class Home {}
+export class Home {
+  private authService = inject(AuthService);
+  protected logoutHandler() {
+    this.authService.logout();
+  }
+}
