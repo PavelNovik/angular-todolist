f784076a9064acf9509948ccc82803b70f7f183d
Pavel Novik <brn2befree@gmail.com>
2026-01-20 16:31:00 +0300

    create login component and add service for it

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'tl-login',
@@ -8,6 +9,7 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrl: './login.scss',
 })
 export class Login {
+  private authService = inject(AuthService);
   loginForm = new FormGroup({
     email: new FormControl('', [
       Validators.pattern('[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,5}$'),
@@ -17,7 +19,8 @@ export class Login {
     rememberMe: new FormControl(false),
   });
   onSubmit(): void {
-    console.log(JSON.stringify(this.loginForm.value));
+    // console.log(JSON.stringify(this.loginForm.value));
+    this.authService.login(this.loginForm.value);
   }
 
   get email(): FormControl {
