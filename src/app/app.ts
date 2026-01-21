9fed09f67cf81e3c651bf9270e2637ce9226a76d
Pavel Novik <brn2befree@gmail.com>
2026-01-21 16:02:01 +0300

    add notification import

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AuthService } from './services/auth.service';
+import { Notify } from './shared/components/notify/notify';
 
 @Component({
   selector: 'tl-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, Notify],
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
 })
