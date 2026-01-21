92643783fee24f43488a0526e132240ef9f9ccd2
Pavel Novik <brn2befree@gmail.com>
2026-01-21 14:44:59 +0300

    make guard async

--- src/app/guards/auth-guard.ts
+++ src/app/guards/auth-guard.ts
@@ -2,16 +2,35 @@ import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const authGuard: CanActivateFn = async (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
-  const isAuth = authService.isAuth;
-  if (!isAuth) {
+
+  // const isAuth = authService.isAuth$;
+  // if (!isAuth) {
+  //   router.navigate(['/login'], {
+  //     queryParams: {
+  //       isAuth: false,
+  //     },
+  //   });
+  // }
+  // return isAuth;
+  await authService.me();
+  if (!authService.isAuth) {
     router.navigate(['/login'], {
       queryParams: {
         isAuth: false,
       },
     });
+    return false;
   }
-  return isAuth;
+  return authService.isAuth;
+
+  // return authService.isAuth$.pipe(
+  //   tap((isAuth) => {
+  //     if (!isAuth) {
+  //       router.navigate(['/login'], { queryParams: { isAuth: false } });
+  //     }
+  //   }),
+  // );
 };
